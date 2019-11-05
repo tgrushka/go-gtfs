639406046083c2df8d38c7b7264813e88d79e143
Otto Söderholm <otto@weasel.fi>
2019-11-05 10:05:53 +0200

    changed stop sequence from string to int

--- models.go
+++ models.go
@@ -57,7 +57,7 @@ type Stop struct {
 // StopTime -
 type StopTime struct {
 	StopID       string  `csv:"stop_id"`
-	StopSeq      string  `csv:"stop_sequence"`
+	StopSeq      int     `csv:"stop_sequence"`
 	StopHeadSign string  `csv:"stop_headsign"`
 	TripID       string  `csv:"trip_id"`
 	Shape        float64 `csv:"shape_dist_traveled"`
